1e790ea4ca1360f6cb9cb5394d11c2ccfbb07c99
diwasht007 <diwashtiwari007@gmail.com>
2024-02-29 13:23:57 +1100

    django-ecommerce

---
+++ carts/context_processors.py
@@ -0,0 +1,20 @@
+from .models import Cart, ItemInCart
+from .views import _cart_id
+
+def counter(request):
+    cart_count = 0
+    if 'admin' in request.path:
+        return {}
+    else:
+        try:
+            cart = Cart.objects.filter(cart_id=_cart_id(request))
+            if request.user.is_authenticated:
+                cart_items = ItemInCart.objects.all().filter(user=request.user)
+            else:
+                cart_items = ItemInCart.objects.all().filter(cart=cart[:1])#one cart_id
+            for cart_item in cart_items:
+                cart_count += cart_item.quantity
+        except Cart.DoesNotExist:
+            cart_count = 0
+    return dict(cart_count=cart_count)
+                
